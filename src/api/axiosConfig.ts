d0c09e5b43c08335549d1fc9ef8790e421a38e2e
alassane <alassanediallozig@gmail.com>
2026-05-08 17:07:45 +0200

    config: add env production and fix baseURL

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '', // ← vide, Vite proxy gère la redirection
+  baseURL: import.meta.env.VITE_API_URL || '',
   headers: {
     'Content-Type': 'application/json',
   },
